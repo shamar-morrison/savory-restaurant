ba2573fed0f00969d8539c1fe67f127ab14f95cf
shamar-morrison <shamar.webdev@gmail.com>
2021-05-09 18:28:35 -0500

    Add about section

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// sticky navbar anim
+const navbar = document.querySelector('.navbar');
+const homeSec = document.querySelector('#home');
+
+const stickyNav = function (event) {
+	const [entry] = event;
+	// console.log('entry', entry);
+	if (!entry.isIntersecting) {
+		// if not intersecting
+		navbar.style.transform = 'translateY(0)';
+		navbar.style.borderRadius = '0 0 10px 10px';
+	} else {
+		navbar.style.transform = 'translateY(18px)';
+		navbar.style.borderRadius = '10px';
+	}
+};
+
+const navbarObs = new IntersectionObserver(stickyNav, { threshold: 0.8 });
+navbarObs.observe(homeSec);
