e63c792a21bb6a47ec2d3c5b336d463dc94f02a7
shamar-morrison <shamar.webdev@gmail.com>
2021-05-25 20:12:55 -0500

    v1.0.1

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	plugins: [
-		require('postcss-uncss')({
-			html: ['./index.html'],
-		}),
+		// require('postcss-uncss')({
+		// 	html: ['./index.html'],
+		// }),
 		require('autoprefixer')({}),
 	],
 };
