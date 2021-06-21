2f32a6b0a2010f9325a7c7bbb7940ac7be275698
shamar-morrison <shamar.webdev@gmail.com>
2021-06-20 20:43:38 -0500

    Add loading=lazy to img tags

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -12839,7 +12839,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55658" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51558" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
